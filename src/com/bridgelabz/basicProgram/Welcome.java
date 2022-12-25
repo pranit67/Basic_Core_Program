11eae46273debfe372e45ed01b790499dc77a5ff
pranit67 <pranit67@gmail.com>
2022-12-25 18:08:36 +0530

    Added : Code for Welcome Message

---
+++ src/com/bridgelabz/basicProgram/Welcome.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.basicProgram;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Basic Core Program...!");
+    }
+}
