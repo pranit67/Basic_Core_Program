ee94d53a53399779753fb70db4a85bf81cb5abfc
pranit67 <pranit67@gmail.com>
2022-12-25 18:33:03 +0530

    Added : Code for Power Of Two

---
+++ src/com/bridgelabz/basicProgram/PowerOfTwo.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.basicProgram;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter The Range :");
+        int range = scan.nextInt();
+        int powerOfTwo = 1;
+        for (int i = 1; i <= range; i++) {
+            powerOfTwo = 2 * powerOfTwo;
+            System.out.println("Power Of Two at " + i + " -> " + powerOfTwo);
+        }
+    }
+
+}
+
