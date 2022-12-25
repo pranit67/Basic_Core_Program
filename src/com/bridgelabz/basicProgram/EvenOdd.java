27645dd27d54779af0e84c96cccc99ca9e8ab085
pranit67 <pranit67@gmail.com>
2022-12-25 19:24:51 +0530

    Added : Code for Number is Even Or Odd

---
+++ src/com/bridgelabz/basicProgram/EvenOdd.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.basicProgram;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner scan  = new Scanner(System.in);
+        System.out.println("Insert the number");
+        int number = scan.nextInt();
+        if (number % 2 ==0){
+            System.out.println("even");
+        }else {
+            System.out.println("odd");
+        }
+    }
+}
